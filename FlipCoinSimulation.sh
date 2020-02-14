5d5e47f736eb6fbae6a9e42432c1538baf193b70
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-14 09:37:37 +0530

    ADDED : Start Flip Coin Simulator And Display Heads Or Tails

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
 
 echo " ****************************** WELCOME TO FLIP COIN SIMULATION ****************************** "
+
+#CONSTANT
+IS_FLIP=0
+
+#STORE RANDOM VALUES 1 OR 0 FOR COIN FLIP
+randomFlip=$(( RANDOM % 2 ))
+
+#CHECK COIN FLIP IS HEADS OR TAILS
+if [ $randomFlip -eq $IS_FLIP ]
+then
+	echo Heads
+else
+	echo Tails
+fi
