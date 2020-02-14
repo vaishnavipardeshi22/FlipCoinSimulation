c68cb45b070211703cebcd51833b57cd37227aeb
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-14 10:54:39 +0530

    ADDED : Store Singlet Combination in Dictionary and Calculate Percentage of Singlet Combination

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -5,13 +5,29 @@ echo " ****************************** WELCOME TO FLIP COIN SIMULATION **********
 #CONSTANT
 IS_FLIP=0
 
-#STORE RANDOM VALUES 1 OR 0 FOR COIN FLIP
-randomFlip=$(( RANDOM % 2 ))
+#VARIABLES
+headCount=0
+tailCount=0
 
-#CHECK COIN FLIP IS HEADS OR TAILS
-if [ $randomFlip -eq $IS_FLIP ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+#DECLARE DICTIONARY TO STORE SINGLET COMBINATION OF FLIP COIN
+declare -A singletFlip
+
+#TAKE USER INPUT FOR NUMBER OF COIN FLIPS
+read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
+
+#STORE HEAD COUNT AND TAIL COUNT IN DICTIONARY
+for(( flip=0; flip<$numberOfCoinFlip; flip++ ))
+do
+	randomFlip=$(( RANDOM % 2 ))
+
+	if [ $randomFlip -eq $IS_FLIP ]
+	then
+		singletFlip[HEAD]=$((++headCount))
+	else
+		singletFlip[TAIL]=$((++tailCount))
+	fi
+done
+
+#PERCENTAGE OF SINGLET COMBINATION FOR HEAD AND TAIL
+singletHeadPercentage=`echo " scale=2; $headCount / $numberOfCoinFlip * 100" | bc`
+singletTailPercentage=`echo " scale=2; $tailCount / $numberOfCoinFlip * 100" | bc`
