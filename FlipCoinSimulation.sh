e5e08c7c57f1ead43b31b9329c8cd985536e290a
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-17 14:54:02 +0530

    ADDED : Sort Function For Sorting Singlet, Doublet and Triplet Combination and Give Winning Combination

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -4,17 +4,23 @@ echo " ****************************** WELCOME TO FLIP COIN SIMULATION **********
 
 #CONSTANT
 IS_FLIP=0
-NUMBER_OF_COIN=3
+SINGLET=1
+DOUBLET=2
+TRIPLET=3
 
-#DECLARE DICTIONARY TO STORE TRIPLET COMBINATION OF FLIP COIN
+#DECLARE DICTIONARY TO STORE SINGLET, DOUBLET AND TRIPLET COMBINATION OF FLIP COIN
+declare -A singletFlip
+declare -A doubletFlip
 declare -A tripletFlip
 
 #TAKE USER INPUT FOR NUMBER OF COIN FLIPS
 read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
 
-#STORE TRIPLET COMBINATION IN DICTIONARY
-function triplet()
+#STORE COMBINATIONS IN DICTIONARY
+function coinFlip()
 {
+	local NUMBER_OF_COIN=$1
+
 	for(( flip=0; flip<$numberOfCoinFlip; flip++ ))
 	do
 		for(( coin=0; coin<$NUMBER_OF_COIN; coin++ ))
@@ -28,22 +34,100 @@ function triplet()
 				coinSide+=T
 			fi
 		done
-		((tripletFlip[$coinSide]++))
+
+		if [ $NUMBER_OF_COIN -eq $SINGLET ]
+		then
+			((singletFlip[$coinSide]++))
+		elif [ $NUMBER_OF_COIN -eq $DOUBLET ]
+		then
+			((doubletFlip[$coinSide]++))
+		else
+			((tripletFlip[$coinSide]++))
+		fi
 		coinSide=""
 	done
 }
 
-#CALCULATE PERCENTAGE OF TRIPLET COMBINATION
+#CALCULATE PERCENTAGE OF COMBINATIONS
 function calculatePercentage()
 {
-	for i in ${!tripletFlip[@]}
+	local NUMBER_OF_COIN=$1
+
+	if [ $NUMBER_OF_COIN -eq $SINGLET ]
+	then
+		for index in ${!singletFlip[@]}
+		do
+			singletFlip[$index]=`echo "scale=2; ${singletFlip[$index]} * 100 / $numberOfCoinFlip" | bc`
+		done
+	elif [ $NUMBER_OF_COIN -eq $DOUBLET ]
+	then
+		for index in ${!doubletFlip[@]}
+		do
+			doubletFlip[$index]=`echo "scale=2; ${doubletFlip[$index]} * 100 / $numberOfCoinFlip" | bc`
+		done
+	else
+		for index in ${!tripletFlip[@]}
+		do
+			tripletFlip[$index]=`echo "scale=2; ${tripletFlip[$index]} * 100 / $numberOfCoinFlip" | bc`
+		done
+	fi
+}
+#FUNCTION TO CONVERT DICTIONARY TO ARRAY
+function dictionaryConvert()
+{
+	arrayKeyValue=("$@")
+	local NO_OF_COMBINATIONS=$((${#arrayKeyValue[@]}/2))
+
+	for(( index=0; index<$NO_OF_COMBINATIONS; index++))
 	do
-		tripletFlip[$i]=`echo "scale=2; ${tripletFlip[$i]} * 100 / $numberOfCoinFlip" | bc`
+		resultArray[$index]=${arrayKeyValue[$index]}:${arrayKeyValue[$NO_OF_COMBINATIONS+$index]}
 	done
+	echo ${resultArray[@]}
 }
 
-#FUNCTION CALL TO FIND TRIPLET COMBINATION
-triplet
+#FUNCTION FOR SORTING SINGLET, DOUBLET AND TRIPLET COMBINATION
+function sortCombination()
+{
+	arrayKeyValues=("$@")
+	local NO_OF_COMBINATIONS=${#arrayKeyValues[@]}
 
-#FUNCTION CALL TO CALCULATE PERCENTAGE OF TRIPLET COMBINATION
-calculatePercentage
+	for(( i=0; i<$NO_OF_COMBINATIONS; i++ ))
+	do
+		for(( j=0; j<$(($NO_OF_COMBINATIONS-1)); j++ ))
+		do
+			firstCombination=$(echo ${arrayKeyValues[j]} | awk -F: '{print $2}')
+			secondCombination=$(echo ${arrayKeyValues[j+1]} | awk -F: '{print $2}')
+
+			if (( $(echo "$firstCombination < $secondCombination" | bc -l) ))
+			then
+				temp=${arrayKeyValues[j]}
+				arrayKeyValues[j]=${arrayKeyValues[j+1]}
+				arrayKeyValues[j+1]=$temp
+			fi
+		done
+	done
+	echo ${arrayKeyValues[@]}
+}
+
+#FUNCTION CALL TO FIND SINGLET, DOUBLET AND TRIPLET COMBINATION
+coinFlip $SINGLET
+coinFlip $DOUBLET
+coinFlip $TRIPLET
+
+#FUNCTION CALL TO CALCULATE PERCENTAGE OF SINGLET, DOUBLET AND TRIPLET COMBINATION
+calculatePercentage $SINGLET
+calculatePercentage $DOUBLET
+calculatePercentage $TRIPLET
+
+#FUNCTION CALL TO SORT AND GET WINNING COMBINATION FOR SINGLET, DOUBLET AND TRIPLET
+arraySinglet="$(dictionaryConvert ${!singletFlip[@]} ${singletFlip[@]})"
+arraySinglet=($(sortCombination $arraySinglet))
+singletWinningCombination=${arraySinglet[0]}
+
+arrayDoublet="$(dictionaryConvert ${!doubletFlip[@]} ${doubletFlip[@]}})"
+arrayDoublet=($(sortCombination $arrayDoublet))
+doubletWinningCombination=${arrayDoublet[0]}
+
+arrayTriplet="$(dictionaryConvert ${!tripletFlip[@]} ${tripletFlip[@]})"
+arrayTriplet=($(sortCombination $arrayTriplet))
+tripletWinningCombination=${arrayTriplet[0]}
